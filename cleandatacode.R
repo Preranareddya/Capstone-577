45d7a9170d0b42d63bdda58a06702161ba7486c6
Preranareddya <39816285+Preranareddya@users.noreply.github.com>
2019-03-24 21:23:24 -0700

    Update cleandatacode.R

--- cleandatacode.R
+++ cleandatacode.R
@@ -295,7 +295,10 @@ for (iterator in 1:sum(yIndex))
       templist <- c()
       templist <- templistNoGeo
     }
-
+    set.seed(123) # set a seed so that people get the same 75% next time we run the same code
+    id.train = sample(1:nrow(templist), nrow(templist)*.75) # nrow() gives number of rows
+    id.test = setdiff(1:nrow(templist), id.train) # setdiff gives the set difference
+    
     
     res <- cor(templist)
     
